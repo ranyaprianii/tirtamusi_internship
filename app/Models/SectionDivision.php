bf10d697cae68545245e87bed1302103db962738
unknown <ranyap3@gmail.com>
2023-05-14 15:30:30 +0700

    first commit

---
+++ app/Models/SectionDivision.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SectionDivision extends Model
+{
+    use HasFactory;
+}
