f5cecb2e0c2b50fb7304e4e6e666bad14c2a7a1e
unknown <ranyap3@gmail.com>
2023-06-11 10:18:45 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="#"><img src="{{ asset('mazer-admin/assets/images/tirtamusi.png') }}" alt="TirtaMusi Internship"   ></a>
+                    <a href="#"><img src="{{ asset('mazer-admin/assets/images/logo1.png') }}" alt="TirtaMusi Internship"   ></a>
                 </div>
                 <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
